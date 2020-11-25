cfbd05cd225271e0dad2d3df77221c7f7a875eb7
Waruka Shotta <alkashotta@gmail.com>
2020-11-26 00:22:27 +0800

    Heroku Setup

---
+++ cmd/convertor/ai.go
@@ -0,0 +1,102 @@
+package convertor
+
+import (
+	"encoding/json"
+	fmt "fmt"
+	"github.com/aws/aws-sdk-go/service/textract"
+	"io/ioutil"
+	"os"
+)
+
+func Run() {
+	jsonFile, err := os.Open("/Users/alka/Desktop/accounting/server/assets/results.json")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Successfully Opened users.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var result []textract.Block
+	json.Unmarshal([]byte(byteValue), &result)
+	blocksMap := make(map[string]textract.Block)
+	var tableBlocks []textract.Block
+	for _, s := range result {
+		blocksMap[*s.Id] = s
+		if *s.BlockType == "TABLE" {
+				tableBlocks = append(tableBlocks, s)
+		}
+	}
+
+	fmt.Println(len(tableBlocks))
+
+
+	row := make(map[int64]interface{})
+	rowIndex := make(map[int]interface{})
+	var sentence string
+	var main []string
+	for i,s := range tableBlocks {
+		_,to := rowIndex[i]
+		if !to {
+			rowIndex[i] = ""
+			row = make(map[int64]interface{})
+		}
+		for _,relationship := range s.Relationships {
+			if *relationship.Type == "CHILD" {
+				for _, childId := range relationship.Ids {
+					cell := blocksMap[*childId]
+					if *cell.BlockType == "CELL" {
+						//fmt.Println(rowIndex, *cell.RowIndex)
+
+
+							_, ok := row[*cell.RowIndex]
+							if !ok {
+
+								sentence = ""
+								main = nil
+							}
+
+							sentence += getText(cell, blocksMap)
+
+							row[*cell.RowIndex] = append(main, sentence)
+						}
+
+
+					}
+
+				}
+
+			}
+
+		rowIndex[i] = row
+
+	}
+
+fmt.Println(rowIndex)
+}
+
+
+
+func getText(cell textract.Block, blocksMap map[string]textract.Block)string{
+	text := ""
+	if len(cell.Relationships) > 0 {
+		for _, relationship := range cell.Relationships{
+			if *relationship.Type == "CHILD" {
+				for _, childId := range relationship.Ids {
+					word := blocksMap[*childId]
+					if *word.BlockType == "WORD" {
+						text += *word.Text + " "
+					}
+					if *word.BlockType == "SELECTION_ELEMENT" {
+						if *word.SelectionStatus == "SELECTED"{
+							text +=  "X "
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return text + ","
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/varukashotta/easyacc
+
+go 1.14
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/aws/aws-sdk-go v1.35.33
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
